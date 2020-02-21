c21398de8e0db28313fb00d3d60a77d04e7378de
Michael Baart <michael.baart96@gmail.com>
2020-02-20 23:53:20 -0800

    Add linting and serverless framework

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint', 
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: [
+    'prettier',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "prettier/prettier": ["error"]
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lambda-snake
 Express.js application hosted on AWS Lambda
+
+## Installing
+
+Clone git repo
+
+Run `npm install`
+
+## Deploying
+
+Run `npm run lint` and `npm run deploy`
